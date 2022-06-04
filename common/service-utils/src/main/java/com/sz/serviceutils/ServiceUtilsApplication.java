33bc6d1b3385d59b02a87626ad5b4c7101d221ce
zs357 <zhengshi1998@foxmail.com>
2022-06-04 14:05:27 +0800

    Building up project

---
+++ common/service-utils/src/main/java/com/sz/serviceutils/ServiceUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.sz.serviceutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceUtilsApplication.class, args);
+    }
+
+}
